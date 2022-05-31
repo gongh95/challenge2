5b3b4f7e038db1b7f8b60046ed54a8691cb73310
gongh95 <gonzalo.simon.1995@gmail.com>
2022-05-31 15:52:33 -0300

    añadido permiso para jugar y array de letras erradas

--- funcion-btns.js
+++ funcion-btns.js
@@ -4,7 +4,10 @@ const pantallaInicio = document.getElementById('inicio'), //declaracion e inicia
 
 const textArea = document.getElementById('textarea-palabra'),
       modalError = document.getElementById('modal-error'),
-      modalExito = document.getElementById('modal-exito');
+      modalExito = document.getElementById('modal-exito'),
+      palabraADescubrir = document.getElementById('palabra-a-descubrir'),
+      letrasErradas = document.getElementById('letras-equivocadas'),
+      containerPalabras = document.getElementById('container-palabras');
 
 const iniciarJuegoBtn = document.getElementById('iniciar-btn'), //declaracion e inicializacion de botones
       irPantallaAgregarPalabraBtn = document.getElementById('agregar-btn'),
@@ -20,6 +23,8 @@ palabras = palabrasDefault.map(palabra => {
     return palabra; //agregar palabras default
 });
 
+let palabraParaJugar = [];
+
 function agregarPalabra() {
     palabras.push(textArea.value);
 }
@@ -29,15 +34,23 @@ function agregarPalabra() {
 iniciarJuegoBtn.addEventListener('click', () => {
     pantallaInicio.classList.toggle('hidden');
     pantallaJuego.classList.toggle('hidden');
+    permitirJuego(); // permiso para empezar a jugar
     armarHorca(); // se arma la horca con boton iniciar
+    generarIndiceRandom(); // se genera nueva palabra aleatoria cada vez que se inicia el juego
+    // console.log(palabraElegida(indicePalabraRandom)); // se consologea la palabra a jugar
+    palabraParaJugar = palabraElegida(indicePalabraRandom).split(""); // se pasa a un array las letras de la palabra a jugar
+    palabraADescubrir.innerHTML = palabraParaJugar.join(" "); // muestra palabra a descubrir
+    crearSpan();
 });
 
 irPantallaAgregarPalabraBtn.addEventListener('click', () => {
     pantallaInicio.classList.toggle('hidden');
     pantallaAgregarPalabra.classList.toggle('hidden');
+    permitirJuego();
 });
 
 guardarPalabra.addEventListener('click', () => {
+    permitirJuego();
     const validar = textArea.value;
     const mayusculas = new RegExp(/(^[A-Z]{2,8})$/);
 
@@ -61,14 +74,25 @@ guardarPalabra.addEventListener('click', () => {
 volverBtn.addEventListener('click', () => {
     pantallaAgregarPalabra.classList.toggle('hidden');
     pantallaInicio.classList.toggle('hidden');
+    permitirJuego();
 });
 
 reiniciarJuego.addEventListener('click', () => {
     limpiar(); // se vacia el canvas
     armarHorca(); // se arma el diseño
+    generarIndiceRandom();
+    // console.log(palabraElegida(indicePalabraRandom));
+    palabraParaJugar = palabraElegida(indicePalabraRandom).split("");
+    palabraADescubrir.innerHTML = palabraParaJugar.join(" ");
+    limpiarArrayErrores();
+    limpiarSpan();
+    crearSpan();
 });
 
 finalizarJuego.addEventListener('click', () => {
     pantallaInicio.classList.toggle('hidden');
     pantallaJuego.classList.toggle('hidden');
+    limpiarArrayErrores();
+    limpiarSpan();
+    permitirJuego();
 });
---
+++ juego.js
@@ -0,0 +1,66 @@
+let indicePalabraRandom;
+
+function generarIndiceRandom() {
+    indicePalabraRandom = Math.floor(Math.random()*palabras.length);
+}
+
+function palabraElegida(indice) {
+    return palabras[indice];
+}
+
+// crear span por cada letra
+function crearSpan() {
+    palabraParaJugar.map((letra) => {
+        const span = document.createElement('p');
+        const caracter = document.createTextNode(letra);
+        span.appendChild(caracter);
+        span.classList.add('spancito');
+        containerPalabras.appendChild(span);
+    });
+}
+
+function limpiarSpan() { //limpiar span con letras de la palabra
+    containerPalabras.innerHTML = "";
+}
+
+let permitidoJugar = false; // permiso para jugar
+
+function letraCorrecta(unaLetra) {
+
+}
+
+function letraIncorrecta(unaLetra) {
+
+}
+
+
+// SECCION DE PRESSKEY
+
+const caracteresPermitidos = new RegExp(/(^[a-z]{0,16})$/);
+let arrayLetrasErradas = [];
+
+function permitirJuego() {
+    if (pantallaJuego.classList.contains("hidden")) {
+        permitidoJugar = false;
+    }
+    else {
+        permitidoJugar = true;
+    }
+}
+
+function limpiarArrayErrores() {
+    arrayLetrasErradas = [];
+    letrasErradas.innerText = arrayLetrasErradas;
+}
+
+document.addEventListener('keydown', (e) => {
+    if (permitidoJugar && e.key.match(caracteresPermitidos)) {
+        let teclaPresionada = e.key.toUpperCase(); // se pasa la letra permitida a mayuscula
+        //console.log(teclaPresionada); // se consologea la letra presionada
+        arrayLetrasErradas.push(teclaPresionada); // se agrega el caracter valido al array de errores
+        letrasErradas.innerText = arrayLetrasErradas;
+    }
+    else {
+        console.log("Esa tecla no se va a mostrar");
+    }
+})
